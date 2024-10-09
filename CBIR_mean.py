197ee0e39cd1cb5539c1d23a9db06f35cec21e39
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2024-10-09 22:27:12 +0530

    Update CBIR_mean.py

--- CBIR_mean.py
+++ CBIR_mean.py
@@ -4,7 +4,7 @@ import os
 import matplotlib.pyplot as plt
 
 def calculate_mean(image_path):
-    # Read the image
+    # Read the image     
     image = cv2.imread(image_path)
     if image is None:
         raise ValueError(f"Image at {image_path} not found or could not be opened.")
