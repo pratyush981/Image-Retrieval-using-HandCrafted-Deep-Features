d7d1678b80b3c254b0242d40639b89c98ddee6d1
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2025-01-31 12:15:48 +0530

    Update mean_calc.py

--- mean_calc.py
+++ mean_calc.py
@@ -3,7 +3,7 @@ import numpy as np
   
 def calculate_mean(image_path):
     # Read kar rahe hain image ko 
-    image = cv2.imread(image_path)
+    image = cv2.imread(image_path) 
     if image is None:
         raise ValueError(f"Image at {image_path} not found or could not be opened.")
         
