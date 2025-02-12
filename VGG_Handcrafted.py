4e783fa7f1d5520c756376447ae5d382ea35bbaf
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2025-02-12 20:52:24 +0530

    Update VGG_Handcrafted.py

--- VGG_Handcrafted.py
+++ VGG_Handcrafted.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from skimage.feature import local_binary_pattern
 from tensorflow.keras.applications import VGG16  
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing import image 
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.models import Model
 from sklearn.preprocessing import StandardScaler
