71a3203543d0005224d38324b60dc4dd8f8c8108
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2025-02-17 12:56:36 +0530

    Update mean_calc.py

--- mean_calc.py
+++ mean_calc.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
   
-def calculate_mean(image_path):
+def calculate_mean(image_path): 
     # Read kar rahe hain image ko  
     image = cv2.imread(image_path)
     if image is None:
