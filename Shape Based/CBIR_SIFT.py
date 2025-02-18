b3c468c1ee206eaa97c63e581c802da8295cc61d
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2025-02-18 13:14:05 +0530

    Update CBIR_SIFT.py

--- Shape Based/CBIR_SIFT.py
+++ Shape Based/CBIR_SIFT.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 import matplotlib.pyplot as plt
- 
+  
 def calculate_sift_features(image_path): 
      
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
