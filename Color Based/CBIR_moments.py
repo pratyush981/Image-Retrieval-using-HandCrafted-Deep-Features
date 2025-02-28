3347598df9a67bbea17252068de6e1c3a7f3e589
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2025-02-28 20:18:20 +0530

    Update CBIR_moments.py

--- Color Based/CBIR_moments.py
+++ Color Based/CBIR_moments.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import os
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 from scipy.stats import skew
  
 def calculate_color_moments(image_path):
